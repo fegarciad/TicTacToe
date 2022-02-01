2529642f8e39b1f489539dd3a4b639d97fa7921c
Fernando Garcia <fernandogarciadoffing@gmail.com>
2022-02-01 16:05:42 -0300

    Move functions in main to game class. Implement undo move.

--- Game.cpp
+++ Game.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include "Game.h"
 
-void Game::printBoard(std::string board[][3])
-{
+void Game::set_players() {
+    bool playerX;
+    bool playerO;
+    system("CLS");
+    std::cout << std::endl << "           TIC-TAC-TOE" << std::endl;
+    std::cout << std::endl << std::endl <<"  Player X: human [1] or cpu [0]  ";
+    std::cin >> Game::playerX;
+    system("CLS");
+    std::cout << std::endl << "           TIC-TAC-TOE" << std::endl;
+    std::cout << std::endl << std::endl << "  Player O: human [1] or cpu [0]  ";
+    std::cin >> Game::playerO;
+}
+
+void Game::main_screen() {
+    system("CLS");
+    std::cout << std::endl << "           TIC-TAC-TOE" << std::endl;
+    Game::print_board();
+    std::cout << "   Enter row and column separated" << std::endl << "   by a space (0 0 to undo move): ";
+}
+
+void Game::print_board() {
     std::cout << std::endl;
     for (int i = 0; i < 5; i++) {
-        std::cout << "    ";
+        std::cout << "            ";
         for (int j = 0; j < 3; j++) {
             if (i % 2 == 1) {
                 std::cout << "---";
@@ -13,10 +32,10 @@ void Game::printBoard(std::string board[][3])
             }
             else {
                 if (j < 2) {
-                    std::cout << board[i/2][j] << " | ";
+                    std::cout << Game::board[i/2][j] << " | ";
                 }
                 else {
-                    std::cout << board[i/2][j];
+                    std::cout << Game::board[i/2][j];
                 }
             }
         }
@@ -25,10 +44,24 @@ void Game::printBoard(std::string board[][3])
     std::cout << std::endl;
 }
 
-bool Game::validMove(std::string board[][3], int x, int y)
-{
+void Game::print_result(bool is_draw) {
+    int place_holder;
+    system("CLS");
+    if (is_draw) {
+        std::cout << std::endl << "            No winner" << std::endl;
+    }
+    else {
+        char winner = (not Game::is_x_turn) ? 'X' : 'O';
+        std::cout << std::endl << "         " << winner << " is the winner!" << std::endl;
+    }
+    Game::print_board();
+    std::cout << std::endl << "  ";
+    system("PAUSE");
+}
+
+bool Game::valid_move(int x, int y) {
     if ((x < 4) && (x > 0) && (y < 4) && (y > 0)) {
-        if (board[x-1][y-1] == "-") {
+        if (Game::board[x-1][y-1] == "-") {
             return true;
         }
         else {
@@ -40,33 +73,46 @@ bool Game::validMove(std::string board[][3], int x, int y)
     }
 }
 
-void Game::playMove(std::string board[][3], int x, int y, bool turnX)
-{
+void Game::play_move(int x, int y) {
     char c = 'x';
 
-    if (turnX) {
+    if (is_x_turn) {
         c = 'x';
     }
     else {
         c = 'o';
     }
-    board[x - 1][y - 1] = c;
+
+    Game::board[x - 1][y - 1] = c;
+    Game::is_x_turn = not Game::is_x_turn;
+    Game::turns++;
+    Game::row_history.push_back(x);
+    Game::column_history.push_back(y);
 }
 
-bool Game::checkWinner(std::string board[][3])
-{
+void Game::undo_move() {
+    if (Game::row_history.size() > 0 && Game::column_history.size() > 0) {
+        Game::board[Game::row_history.back() - 1][Game::column_history.back() - 1] = "-";
+        Game::is_x_turn = not Game::is_x_turn;
+        Game::turns--;
+        Game::row_history.pop_back();
+        Game::column_history.pop_back();
+    }
+}
+
+bool Game::check_winner() {
     for (int i = 0; i < 3; i++) {
-        if ((board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) && ((board[i][0] == "x") || (board[i][0] == "o"))) {
+        if ((Game::board[i][0] == Game::board[i][1]) && (Game::board[i][1] == Game::board[i][2]) && ((Game::board[i][0] == "x") || (Game::board[i][0] == "o"))) {
             return true;
         }
-        if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i]) && ((board[0][i] == "x") || (board[0][i] == "o"))) {
+        if ((Game::board[0][i] == Game::board[1][i]) && (Game::board[1][i] == board[2][i]) && ((Game::board[0][i] == "x") || (Game::board[0][i] == "o"))) {
             return true;
         }
     }
-    if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && ((board[0][0] == "x") || (board[0][0] == "o"))) {
+    if ((Game::board[0][0] == Game::board[1][1]) && (Game::board[1][1] == Game::board[2][2]) && ((Game::board[0][0] == "x") || (Game::board[0][0] == "o"))) {
         return true;
     }
-    if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && ((board[0][2] == "x") || (board[0][2] == "o"))) {
+    if ((Game::board[0][2] == Game::board[1][1]) && (Game::board[1][1] == Game::board[2][0]) && ((Game::board[0][2] == "x") || (Game::board[0][2] == "o"))) {
         return true;
     }
     return false;
--- Game.h
+++ Game.h
@@ -1,15 +1,27 @@
 #pragma once
+#include <vector>
 
-class Game
+class Game 
 {
 	public:
-		void printBoard(std::string board[][3]);
-		bool validMove(std::string board[][3], int x, int y);
-		void playMove(std::string board[][3], int x, int y, bool turnX);
-		bool checkWinner(std::string board[][3]);
+		void set_players();
+		void main_screen();
+		void print_board();
+		void print_result(bool is_draw);
+		bool valid_move(int x, int y);
+		void play_move(int x, int y);
+		void undo_move();
+		bool check_winner();
+	
+		bool is_x_turn = true;
+		bool playerX = true;
+		bool playerO = true;
+		int turns = 0;
+		std::vector<int> row_history;
+		std::vector<int> column_history;
 		std::string board[3][3] = {
 			{ "-","-","-" },
 			{ "-","-","-" },
 			{ "-","-","-" }
-			};
+		};
 };
